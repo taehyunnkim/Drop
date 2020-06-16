9e9072a669d29888e1b706448a1c829f684a692c
Eric Kim <kim2000@uw.edu>
2020-06-16 11:27:56 +0900

    Init

---
+++ desktop/src/com/taehyunkim/drop/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.taehyunkim.drop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.taehyunkim.drop.Drop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Drop(), config);
+	}
+}
